73ec24e92bd49058a8bf10b35958a302577ad8fc
MeryAmun <chebesiawah@gmail.com>
2022-01-07 06:56:57 +0100

    what is GPT3 styles

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -8,7 +8,7 @@ export const Header = () => {
   return (
     <div className='gpt3__header section__padding' id='home'>
       <div className='gpt3__header-content'>
-        <h1 className='gradrient__text'>
+        <h1 className='gradient__text'>
           Let’s Build Something amazing with GPT-3 OpenAI
         </h1>
         <p>
