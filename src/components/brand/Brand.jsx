22d4eaa9d0c72c73eacfeba528b532f26ea5ea6a
MeryAmun <chebesiawah@gmail.com>
2022-01-07 06:15:13 +0100

    brand and styles

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,11 +1,27 @@
 import './brand.css'
 
+import { atlassian, dropbox, google, shopify, slack } from './index'
+
 import React from 'react'
 
 export const Brand = () => {
   return (
-    <div>
-      <h2>Brand</h2>
+    <div className='gpt3__brand section__padding'>
+      <div>
+        <img src={google} alt='google' />
+      </div>
+      <div>
+        <img src={slack} alt='slack' />
+      </div>
+      <div>
+        <img src={dropbox} alt='dropbox' />
+      </div>
+      <div>
+        <img src={shopify} alt='shopify' />
+      </div>
+      <div>
+        <img src={atlassian} alt='atlassian' />
+      </div>
     </div>
   )
 }
