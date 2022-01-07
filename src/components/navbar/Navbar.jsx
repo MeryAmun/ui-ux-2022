df0e42c14db9b0d3f3931e60c762fe83db233ac0
MeryAmun <chebesiawah@gmail.com>
2022-01-07 04:24:03 +0100

    navbar done with responsiveness

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -44,27 +44,32 @@ export const Navbar = () => {
         <p>Sign in</p>
         <button type='button'>Sign up</button>
       </div>
-      <div className='gpt3__navbar-menu'></div>
-      {toggleMenu ? (
-        <RiCloseLine
-          color='#fff'
-          size={27}
-          onClick={() => setToggleMenu(false)}
-        />
-      ) : (
-        <RiMenu3Line
-          color='#fff'
-          size={27}
-          onClick={() => setToggleMenu(true)}
-        />
-      )}
-      {toggleMenu && (
-        <div className='gpt3__navbar-menu_container scale-up-center'>
-          <div className='gpt3__navbar-menu_container-links'>
-            <Menu />
+      <div className='gpt3__navbar-menu'>
+        {toggleMenu ? (
+          <RiCloseLine
+            color='#fff'
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color='#fff'
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <div className='gpt3__navbar-menu_container scale-up-center'>
+            <div className='gpt3__navbar-menu_container-links'>
+              <Menu />
+              <div className='gpt3__navbar-menu_container-links-sign'>
+                <p>Sign in</p>
+                <button type='button'>Sign up</button>
+              </div>
+            </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   )
 }
