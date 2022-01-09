c5caef409117eea01e749ed7ba890ef9f5453098
MeryAmun <chebesiawah@gmail.com>
2022-01-09 03:58:26 +0100

    blog and articles

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -2,10 +2,19 @@ import './article.css'
 
 import React from 'react'
 
-export const Article = () => {
+export const Article = ({ imageUrl, title, date }) => {
   return (
-    <div>
-      <h2>article</h2>
+    <div className='gpt3__blog-container_article'>
+      <div className='gpt3__blog-container_article-image'>
+        <img src={imageUrl} alt='article' />
+      </div>
+      <div className='gpt3__blog-container_article-content'>
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full article</p>
+      </div>
     </div>
   )
 }
